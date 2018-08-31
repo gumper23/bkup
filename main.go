6af9cc057e2a639c037e353f90045b0ead857201
Russ Smith <gumper@gumper.com>
2018-08-31 05:37:47 -0700

    Added backup function.

--- main.go
+++ main.go
@@ -44,8 +44,43 @@ func main() {
 		fmt.Printf("%s\n", file)
 	}
 
+	err = backupFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func printUsage() {
 	fmt.Printf("Usage: %s <file_to_backup>\n", os.Args[0])
+}
+
+func backupFile(file string) error {
+	// Generate backup file name.
+	// <filename>.<datetime>
+	t := time.Now().Local()
+	ext := t.Format("20060102150405")
+	dst := file + "." + ext
+
+	// Ensure the backup file doesn't already exist
+	if _, err := os.Stat(dst); os.IsExist(err) {
+		return os.ErrExist
+	}
+
+	in, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
