b5e1cb6c54699396a83e5d1910f80094036543bf
Russ Smith <gumper@gumper.com>
2018-09-02 07:28:19 -0700

    Added kingpin for command line arguments, better logging, and a function for comparing files.

--- main.go
+++ main.go
@@ -12,27 +12,28 @@ import (
 	"sort"
 	"strings"
 	"time"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app  = kingpin.New("bkup", "Rotating backup file utility. Only creates a backup on files that change since the last backup.")
+	num  = app.Flag("num", "Number of rotating backup files.").Default("12").Short('n').Int()
+	file = app.Arg("file", "File to be backed up.").Required().String()
 )
 
 // go build -o bkup.exe -ldflags -H=windowsgui .
 func main() {
-	// Intended to be ran on a schedule of once every five minutes
-	// One hour's worth of backups
-	maxBackupFiles := 12
-
 	// Usage: bkup.exe <file_to_be_backed_up>
-	if len(os.Args) != 2 {
-		printUsage()
-		os.Exit(1)
-	}
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	// Ensure the file to be backed up exists
-	if _, err := os.Stat(os.Args[1]); os.IsNotExist(err) {
-		log.Fatalf("Can't find [%s]: %v\n", os.Args[1], err)
+	if _, err := os.Stat(*file); os.IsNotExist(err) {
+		log.Fatalf("Can't find [%s]: %v\n", *file, err)
 	}
 
 	// Create a log file in the directory of the file to be backed up
-	path, err := filepath.Abs(filepath.Dir(os.Args[1]))
+	path, err := filepath.Abs(filepath.Dir(*file))
 	if err != nil {
 		log.Fatalf("error determining path: [%v]\n", err)
 	}
@@ -43,27 +44,18 @@ func main() {
 	defer logf.Close()
 	log.SetOutput(logf)
 
-	// Calculate the MD5 hash of the file
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	if *num <= 0 {
+		log.Fatalf("num [%d] must be greater than 0", *num)
 	}
-	defer f.Close()
-
-	h := md5.New()
-	if _, err := io.Copy(h, f); err != nil {
-		log.Fatal(err)
-	}
-	f.Close()
 
 	// Search for existing backup files
-	files, err := filepath.Glob(os.Args[1] + ".*")
+	files, err := filepath.Glob(*file + ".*")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Backup files are OTF: <file>.YYYYMMDDHHMMSS (14 digits)
-	restr := fmt.Sprintf("%s\\.[0-9]{14}$", strings.Replace(os.Args[1], `\`, `\\`, -1))
+	restr := fmt.Sprintf("%s\\.[0-9]{14}$", strings.Replace(*file, `\`, `\\`, -1))
 	re, err := regexp.Compile(restr)
 	if err != nil {
 		log.Fatal(err)
@@ -78,36 +70,41 @@ func main() {
 
 	// Create a backup file if one doesn't exist.
 	if len(backups) == 0 {
-		backupFile(os.Args[1])
-		os.Exit(0)
+		dst, err := backupFile(*file)
+		if err != nil {
+			log.Fatalf("error backing up %s: %v\n", *file, err)
+		}
+		log.Printf("Copied [%s] to [%s]\n", *file, dst)
+		return
 	}
 
 	// Compare the last file (most recent) to the argument.
 	sort.Strings(backups)
-	lf, err := os.Open(backups[len(backups)-1])
+
+	// Do nothing if the file and most recent backup are the same.
+	same, err := fileMD5Compare(*file, backups[len(backups)-1])
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error comparing files %v", err)
 	}
-	defer lf.Close()
-
-	lh := md5.New()
-	if _, err := io.Copy(lh, lf); err != nil {
-		log.Fatal(err)
-	}
-	lf.Close()
-
-	// Do nothing if the hashes of the argument and
-	// the most recent backup match
-	if reflect.DeepEqual(h, lh) {
-		os.Exit(0)
+	if same {
+		return
 	}
 
 	// Backup the file.
-	backupFile(os.Args[1])
+	dst, err := backupFile(*file)
+	if err != nil {
+		log.Fatalf("error backing up %s: %v", *file, err)
+	}
+	log.Printf("Copied [%s] to [%s]\n", *file, dst)
 
 	// Delete the oldest backup file if we are at max files.
-	if len(backups) == maxBackupFiles {
-		os.Remove(backups[0])
+	if len(backups) >= *num {
+		err := os.Remove(backups[0])
+		if err != nil {
+			log.Fatalf("error deleting %s: %v\n", backups[0], err)
+		}
+		log.Printf("Deleted [%s]\n", backups[0])
+		return
 	}
 }
 
@@ -117,33 +114,61 @@ func printUsage() {
 	log.Printf(usage)
 }
 
-func backupFile(file string) error {
+func backupFile(file string) (dst string, err error) {
 	// Generate backup file name.
 	// <filename>.<datetime>
 	t := time.Now().Local()
 	ext := t.Format("20060102150405")
-	dst := file + "." + ext
+	dst = file + "." + ext
 
 	// Ensure the backup file doesn't already exist
 	if _, err := os.Stat(dst); os.IsExist(err) {
-		return os.ErrExist
+		return dst, os.ErrExist
 	}
 
 	in, err := os.Open(file)
 	if err != nil {
-		return err
+		return dst, err
 	}
 	defer in.Close()
 
 	out, err := os.Create(dst)
 	if err != nil {
-		return err
+		return dst, err
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, in)
 	if err != nil {
-		return err
+		return dst, err
 	}
-	return out.Close()
+	return dst, out.Close()
+}
+
+func fileMD5Compare(file1, file2 string) (same bool, err error) {
+	f1, err := os.Open(file1)
+	if err != nil {
+		return false, err
+	}
+	defer f1.Close()
+	h1 := md5.New()
+	if _, err := io.Copy(h1, f1); err != nil {
+		return false, err
+	}
+	f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		return false, err
+	}
+	defer f2.Close()
+	h2 := md5.New()
+	if _, err := io.Copy(h2, f2); err != nil {
+		return false, err
+	}
+	f2.Close()
+
+	same = reflect.DeepEqual(h1, h2)
+
+	return
 }
